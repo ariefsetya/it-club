e6e8607df21d7c250740fed0b99c950d0be0a75f
Arief Setya <ariefsetya@live.com>
2016-06-20 13:34:38 +0700

    update fullpost idpengguna

--- resources/views/utils/fullpost.blade.php
+++ resources/views/utils/fullpost.blade.php
@@ -14,7 +14,7 @@
      <br>
      <br>
      <br>
-    <span class="place-right"><span class="mif-calendar"></span> {{ date_format(date_create($data->created_at),"l, d M Y") }} <span class="mif-alarm"></span> {{ date_format(date_create($data->created_at),"h:i A") }} <img style="max-width:16px;max-height:16px;" src="{{ url('images/foto/'.App\User::where('id',$data->idpengguna)->first()['foto']) }}"> <a href="{{ url('people/'.$data->id) }}">{{ $user->namadepan." ".$user->namabelakang }}</a></span>
+    <span class="place-right"><span class="mif-calendar"></span> {{ date_format(date_create($data->created_at),"l, d M Y") }} <span class="mif-alarm"></span> {{ date_format(date_create($data->created_at),"h:i A") }} <img style="max-width:16px;max-height:16px;" src="{{ url('images/foto/'.App\User::where('id',$data->idpengguna)->first()['foto']) }}"> <a href="{{ url('people/'.$data->idpengguna) }}">{{ $user->namadepan." ".$user->namabelakang }}</a></span>
      <br>
     </div>
 </div>
